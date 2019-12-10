b9ed3d01cfc278fb3fcc6e91034df163cbfa371c
jamielhf <569309786@qq.com>
2019-12-11 00:56:26 +0800

    update

--- src/feature/auth/dto/auth.dto.ts
+++ src/feature/auth/dto/auth.dto.ts
@@ -1,17 +1,35 @@
-import { IsString, IsInt, IsEmail } from 'class-validator';
-
+import { IsString, IsInt,Matches, IsEmail,IsNotEmpty,NotEquals } from 'class-validator';
+import {Transform} from 'class-transformer';
+import { IsEqualsThan } from '../../../core/decorators/validator.decorators';
 export class LoginDto {
   readonly username: string;
   readonly password: string;
 }
 
 export class ResgisterDto {
-  @IsString()
+  @IsNotEmpty({
+    message: '用户名不能为空',
+  })
+  @Matches(/^[a-zA-Z0-9\-_]{5, 20}$/i, {
+    message: '用户名不合法',
+  })
   readonly username: string;
-  @IsString()
+  @IsNotEmpty({
+    message: '密码不能为空',
+  })
   readonly password: string;
-  @IsString()
+  @IsNotEmpty({
+    message: '确认密码不能为空',
+  })
+  @IsEqualsThan('password', {
+    message: '两次密码输入不一致。',
+  })
   readonly rePwd: string;
-  @IsEmail()
+  @IsNotEmpty({
+    message: '邮箱不能为空',
+  })
+  @IsEmail({}, {
+    message: '邮箱不合法',
+  })
   readonly email: string;
 }
