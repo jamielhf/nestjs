c4926abebba6ad022008243ba0e614951f951d8e
jamielhf <569309786@qq.com>
2020-04-13 22:58:34 +0800

    feat[article]: update

--- src/feature/article/article.controller.ts
+++ src/feature/article/article.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('article')
-export class ArticleController {}
+export class ArticleController {
+  // 获取文章列表
+  @Get('list')
+  getArticleList(){
+
+  }
+  // 获取文章详情
+  @Get('detail')
+  getArticleDetail(){
+
+  }
+  // 保存文章
+  @Post('save')
+  saveArticle(){
+
+  }
+  // 发布文章
+  @Post('publish')
+  publishArticle(){
+
+  }
+  // 删除文章
+  @Post('delete')
+  deleteArticle(){
+
+  }
+}
